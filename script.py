5a8faab44f7778cc4cef9df43558949ac906968f
Aditi Thakur <137780594+aditithakur0006@users.noreply.github.com>
2025-03-27 17:17:50 +0530

    Update script.py
    it should ask for every a in list

--- script.py
+++ script.py
@@ -1,14 +1,10 @@
-import sys
-
-# List containing values
 my_list = [1, 'b', 'a', 1, 'a', 'd', 1, 'a', 'f', 'a']
 
-# Loop through the list
 for i in my_list:
     if i == 'a':
         print("Approval needed for 'a' in Jenkins.")
-        sys.exit(100)  
+        exit(100)  # Exit with status 100 each time 'a' is found
     else:
-        print("'a' not found, found:", i)
+        print(f"'a' not found, found: {i}")
 
 print("Python script completed.")
